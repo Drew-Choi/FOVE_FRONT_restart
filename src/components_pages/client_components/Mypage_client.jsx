d73fc19c9efdb12380355746b48d2d656ee61da1
Drew <drc920630@gmail.com>
2023-05-01 17:29:53 +0900

    myPage 그리드 작업 중

--- src/components_pages/client_components/Mypage_client.jsx
+++ src/components_pages/client_components/Mypage_client.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../styles/mypage_client.scss';
+import myPage from '../../styles/mypage_client.module.scss';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
@@ -8,152 +8,109 @@ export default function Mypage_client() {
   const userNameEncoded = useSelector((state) => state.user.userName);
 
   return (
-    <>
+    <section className={myPage.myPage_container}>
       {/* ACCOUNT 제목 위치 */}
-      <div className="titleArea">
-        <h2 className="subtitle">ACCOUNT</h2>
+      <div className={myPage.titleArea}>
+        <h5 className={myPage.subtitle}>ACCOUNT</h5>
       </div>
-      <div className="account_inner">
-        <div className="account_wrap">
-          {/* 회원 등급 박스 */}
-          <div className="information">
-            <p className="thumbnail">
-              <img src="/images/standard.jpg"></img>
-            </p>
-            <div className="description">
-              <span>
-                저희 쇼핑몰을 이용해주셔서 감사합니다.
-                <br />
-                <span className="name">{userNameEncoded}</span> 님은
-                <span className="namegrade"> STANDARD </span>
-                회원이십니다.
-                <br />
-                <span className="price"> KRW 10,000 이상</span> 구매시
-                <span className="percent"> 5% </span>을 추가할인 받으실 수
-                있습니다. (최대 KRW 9,999,999)
-                <br />
-                <span className="price"> KRW 10,000 이상</span> 구매시
-                <span className="percent"> 5% </span>을 추가적립 받으실 수
-                있습니다. (최대 KRW 9,999,999)
-              </span>
+      <div className={myPage.account_info_container}>
+        {/* 회원 등급 박스 */}
+        <div className={myPage.informations}>
+          <p className={myPage.info_grade}>STANDARD</p>
+          <div className={myPage.info_description}>
+            <span>
+              저희 쇼핑몰을 이용해주셔서 감사합니다.
+              <br />
+              <span className={myPage.info_bold}>{userNameEncoded}</span> 님은
+              <span className={myPage.info_bold}> STANDARD </span>
+              회원이십니다.
+              <br />
+              <span className={myPage.info_bold}> KRW 10,000 이상</span> 구매시
+              <span className={myPage.info_bold}> 5% </span>을 추가할인 받으실
+              수 있습니다. (최대 KRW 9,999,999)
+              <br />
+              <span className={myPage.info_bold}> KRW 10,000 이상</span> 구매시
+              <span className={myPage.info_bold}> 5% </span>을 추가적립 받으실
+              수 있습니다. (최대 KRW 9,999,999)
+            </span>
+          </div>
+        </div>
+        {/* 나의 주문처리 현황 박스 */}
+
+        <div className={myPage.orderstate_box}>
+          <p className={myPage.shipping_info_title}>
+            나의 주문처리 현황 <span>(최근 3개월 기준)</span>
+          </p>
+
+          <a className={myPage.beforeAcount} href="#">
+            <p>입금전</p>
+            <p>0</p>
+          </a>
+
+          <a className={myPage.ready} href="#">
+            <p>배송준비중</p>
+            <p>0</p>
+          </a>
+
+          <a className={myPage.shipping} href="#">
+            <p>배송중</p>
+            <p>0</p>
+          </a>
+
+          <a className={myPage.complete} href="#">
+            <p>배송완료</p>
+            <p>0</p>
+          </a>
+
+          <a className={myPage.cancle} href="#">
+            <p>취소</p>
+            <p>0</p>
+          </a>
+
+          <a className={myPage.change} href="#">
+            <p>교환</p>
+            <p>0</p>
+          </a>
+
+          <a className={myPage.refund} href="#">
+            <p>반품</p>
+            <p>0</p>
+          </a>
+        </div>
+
+        {/* 박스 4개 */}
+        <div className="fourbox">
+          <div className="line_one">
+            <div
+              onClick={() => navigate('/mypage/orderlist')}
+              className="shopmain_order"
+            >
+              <p>주문 조회</p>
+              <span className="material-symbols-outlined">local_shipping</span>
+            </div>
+            <div
+              onClick={() => navigate('/mypage/editInfo')}
+              className="shopmain_profile"
+            >
+              <p>회원 정보 수정</p>
+              <span className="material-symbols-outlined">person</span>
             </div>
           </div>
-          {/* 나의 주문처리 현황 박스 */}
-          <div className="orderstate">
-            <div className="orderstate_box">
-              <div className="title">
-                <div className="title_order">
-                  <h3>
-                    나의 주문처리 현황 <span>(최근 3개월 기준)</span>
-                  </h3>
-                </div>
-              </div>
-              <div className="state">
-                <div className="orderbox">
-                  <ul className="order">
-                    {/* order 리스트 4개 */}
-                    <li>
-                      <a href="#">
-                        <span onClick={() => navigate('#')}>입금전</span>
-                        <strong className="count">
-                          <strong id="count">0</strong>
-                        </strong>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <span onClick={() => navigate('#')}>배송준비중</span>
-                        <strong className="count">
-                          <strong id="count">0</strong>
-                        </strong>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <span onClick={() => navigate('#')}>배송중</span>
-                        <strong className="count">
-                          <strong id="count">0</strong>
-                        </strong>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <span onClick={() => navigate('#')}>배송완료</span>
-                        <strong className="count">
-                          <strong id="count">0</strong>
-                        </strong>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="csbox">
-                  <ul className="cs">
-                    {/* cs 리스트 3개 */}
-                    <li>
-                      <a href="#">
-                        <span onClick={() => navigate('#')}>취소 : </span>
-                        <strong className="count">
-                          <strong id="count">0</strong>
-                        </strong>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <span onClick={() => navigate('#')}>교환 : </span>
-                        <strong className="count">
-                          <strong id="count">0</strong>
-                        </strong>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <span onClick={() => navigate('#')}>반품 : </span>
-                        <strong className="count">
-                          <strong id="count">0</strong>
-                        </strong>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+          <div className="line_two">
+            <div
+              onClick={() => navigate('/mypage/checkAddress')}
+              className="shopmain_address"
+            >
+              <p>배송 주소록</p>
+              <span className="material-symbols-outlined">home</span>
             </div>
-          </div>
-          {/* 박스 4개 */}
-          <div className="fourbox">
-            <div className="line_one">
-              <div
-                onClick={() => navigate('/mypage/orderlist')}
-                className="shopmain_order"
-              >
-                <p>주문 조회</p>
-                <span className="material-symbols-outlined">
-                  local_shipping
-                </span>
-              </div>
-              <div
-                onClick={() => navigate('/mypage/editInfo')}
-                className="shopmain_profile"
-              >
-                <p>회원 정보 수정</p>
-                <span className="material-symbols-outlined">person</span>
-              </div>
-            </div>
-            <div className="line_two">
-              <div
-                onClick={() => navigate('/mypage/checkAddress')}
-                className="shopmain_address"
-              >
-                <p>배송 주소록</p>
-                <span className="material-symbols-outlined">home</span>
-              </div>
-              <div onClick={() => navigate('#')} className="shopmain_mypick">
-                <p>MY STYLING PICK</p>
-                <span className="material-symbols-outlined">favorite</span>
-              </div>
+            <div onClick={() => navigate('#')} className="shopmain_mypick">
+              <p>MY STYLING PICK</p>
+              <span className="material-symbols-outlined">favorite</span>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </section>
   );
 }
