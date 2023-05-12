8210ad8df93237f5d7bf8beefd503f89f32e4033
Drew <drc920630@gmail.com>
2023-05-12 22:55:07 +0900

    로그인 회원가입삭제, 간편로그인만 사용

--- src/components_pages/client_components/Login_client.jsx
+++ src/components_pages/client_components/Login_client.jsx
@@ -3,23 +3,8 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { login } from '../../store/modules/user';
-import '../../styles/login_client.scss';
 import styled from 'styled-components';
-
-const KakaoBTN = styled.img`
-  width: 370px;
-  padding: 20px;
-  cursor: pointer;
-  &:hover {
-    opacity: 0.7;
-  }
-  &:active {
-    opacity: 0.2;
-  }
-  @media screen and (max-width: 280px) {
-    width: 300px;
-  }
-`;
+import loginCss from '../../styles/login_client.module.scss';
 
 export default function Login_client() {
   const dispatch = useDispatch();
@@ -44,115 +29,25 @@ export default function Login_client() {
     const SCOPE = await getKey('SCOPE');
     const REDIRECT_URI = 'http://localhost:3000/login/kakao/callback';
     const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPE}`;
-
     window.location.href = KAKAO_AUTH_URL;
   };
 
-  const loginbtn = useRef();
-
-  const handleKeyPress1 = (e) => {
-    if (e.key === 'Enter') {
-      loginbtn.current?.click();
-    }
-  };
-
-  const handleKeyPress2 = (e) => {
-    if (e.key === 'Enter') {
-      loginbtn.current?.click();
-    }
-  };
-
-  //유저정보 state
-  const userID = useSelector((state) =>
-    state.user.userID === 0 ? 0 : state.user.userID,
-  );
-
-  const loginIdInput = useRef();
-  const loginPwInput = useRef();
-
-  // LOG IN 버튼
-  const loginUser = async () => {
-    try {
-      // 아이디, 비밀번호 값 입력 여부 확인. 없으면 alert 창 뜨게.
-      if (!loginIdInput.current.value) {
-        loginIdInput.current.focus();
-        return alert('아이디를 입력해 주세요.');
-      }
-
-      if (!loginPwInput.current.value) {
-        loginPwInput.current.focus();
-        return alert('비밀번호를 입력해 주세요.');
-      }
-
-      // axios 로 보내기
-      const resLogin = await axios.post('http://localhost:4000/login', {
-        id: loginIdInput.current.value,
-        password: loginPwInput.current.value,
-      });
-
-      // 회원이 아닐 때, 비밀번호가 틀렸을 때 뜨는 에러 메시지 처리
-      if (resLogin.status === 400) alert(resLogin.response.data.message);
-      // 로그인 성공 시, 메시지 처리
-      alert(resLogin.data.message);
-
-      // 로그인이 성공하면 응답 데이터 token 프로퍼티에 accessToken 이 전달 되어 오므로
-      // 로컬 스토리지에 로그인 정보가 저장 된 토큰을 저장
-      // 해당 정보를 통하여 리액트 실행 시, 토큰을 백엔드 서버에 검증하여 자동 로그인을 처리
-      window.localStorage.setItem('token', resLogin.data.token);
-      dispatch(
-        login({
-          id: loginIdInput.current.value,
-        }),
-      );
-      // cartDataReq(loginIdInput.current.value);
-      navigate('/store'); // 로그인 후 이전 페이지로 이동
-    } catch (err) {
-      alert(err.response.data.message);
-    }
-  };
-
   return (
-    <div className="login_client">
-      <div className="login_client_container">
-        <p className="login_title">LOG IN or CREATE ACCOUNT</p>
-        <br />
-        <input
-          type="text"
-          ref={loginIdInput}
-          placeholder="ID (Email Address)"
-          required
-          className="login_input"
-          onKeyDown={(e) => handleKeyPress1(e)}
-        />
-        <br />
-        <input
-          type="password"
-          ref={loginPwInput}
-          placeholder="PW"
-          required
-          className="login_input"
-          onKeyDown={(e) => handleKeyPress2(e)}
-        />
-        <br />
-        <button ref={loginbtn} onClick={loginUser} className="login_btn">
-          LOG IN
-        </button>
-        <br />
-        <button
-          className="login_btn white"
-          onClick={() => navigate(`/register`)}
-        >
-          Create Account
-        </button>
-        <br />
+    <section className={loginCss.login_client}>
+      <div className={loginCss.login_client_container}>
+        <p className={loginCss.login_title}>LOG IN</p>
+        <p className={loginCss.login_desc}>
+          *별도 가입서 작성없이 간편하게 로그인 하세요.
+        </p>
         <a>
-          <KakaoBTN
+          <img
+            className={loginCss.kakaoBTN}
             onClick={kakoHandleClick}
             src="/images/kakao_login_large_wide.png"
             alt="카카오버튼"
           />
         </a>
       </div>
-    </div>
+    </section>
   );
 }
