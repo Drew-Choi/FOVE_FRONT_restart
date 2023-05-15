86da070e621440c64cc48ab4248f506af7d91472
Drew <drc920630@gmail.com>
2023-05-15 12:50:25 +0900

    카카오로그인 로그인 유지 처리 중

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ import OrderList_client from './components_pages/client_components/OrderList_cli
 import Store_NewItems from './components_pages/client_components/Store_NewItems';
 import OrderList_admin from './components_pages/admin_components/OrderList_admin';
 import Kakao_Logout from './components_pages/client_components/Kakao_Logout';
+import Kakao_final from './components_pages/client_components/Kakao_final';
 
 function App() {
   const isLogin = useSelector((state) => state.user.isLogin);
@@ -40,36 +41,36 @@ function App() {
 
   // App 시작 시, 브라우저 로컬 스토리지에 저장 되어 있는 토큰이 있는지를 확인 후,
   // 해당 토큰을 백엔드에 검증. 검증이 되면 바로 로그인 처리 / 안 되면 로그인 페이지로 이동
-  const tokenLoginCheck = async () => {
-    const access_token = sessionStorage.getItem('access_token');
-    if (access_token) {
-      try {
-        // 액세스 토큰을 사용해서 사용자 정보를 가져오는 API 호출
-        const response = await axios.get('https://kapi.kakao.com/v2/user/me', {
-          headers: {
-            Authorization: `Bearer ${access_token}`,
-            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-          },
-        });
-        // API 호출 결과에서 사용자 정보 추출
-        const { kakao_account, properties } = response.data;
-        dispatch(
-          keepLogin({
-            id: kakao_account.email,
-            nameEncoded: properties.nickname,
-            points: 0,
-            isAdmin: false,
-          }),
-        );
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  };
-  // 리액트 앱이 시작 되면 바로 토큰 검증 로직 실행 -> 토큰 로그인 수행
-  useEffect(() => {
-    tokenLoginCheck();
-  }, [isLogin]); // isLogin 값 바뀔 때마다
+  // const tokenLoginCheck = async () => {
+  //   const access_token = sessionStorage.getItem('access_token');
+  //   if (access_token) {
+  //     try {
+  //       // 액세스 토큰을 사용해서 사용자 정보를 가져오는 API 호출
+  //       const response = await axios.get('https://kapi.kakao.com/v2/user/me', {
+  //         headers: {
+  //           Authorization: `Bearer ${access_token}`,
+  //           'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+  //         },
+  //       });
+  //       // API 호출 결과에서 사용자 정보 추출
+  //       const { kakao_account, properties } = response.data;
+  //       dispatch(
+  //         keepLogin({
+  //           id: kakao_account.email,
+  //           nameEncoded: properties.nickname,
+  //           points: 0,
+  //           isAdmin: false,
+  //         }),
+  //       );
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   }
+  // };
+  // // 리액트 앱이 시작 되면 바로 토큰 검증 로직 실행 -> 토큰 로그인 수행
+  // useEffect(() => {
+  //   tokenLoginCheck();
+  // }, [isLogin]); // isLogin 값 바뀔 때마다
 
   const isAdmin = useSelector((state) => state.user.isAdmin);
 
@@ -118,6 +119,7 @@ function App() {
           />
           {/* 로그인 */}
           <Route path="/login" element={<Login_client />} />
+          <Route path="/login/kakao/callback" element={<Kakao_final />} />
           <Route path="/kakao/logout" element={<Kakao_Logout />} />
 
           {/* 로그인 상태여야 이동 가능한 페이지들 */}
--- src/components_pages/client_components/Kakao_final.jsx
+++ src/components_pages/client_components/Kakao_final.jsx
@@ -5,83 +5,83 @@ import { useNavigate } from 'react-router-dom';
 import { login } from '../../store/modules/user';
 
 export default function Kakao_final() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const [info, setInfo] = useState({});
+  // const navigate = useNavigate();
+  // const dispatch = useDispatch();
+  // const [info, setInfo] = useState({});
 
-  const getUserInfo = async () => {
-    const access_token = sessionStorage.getItem('access_token');
-    if (!access_token) {
-      //로그인 되어 있지 않다면, 다시 로그인 페이지로
-      navigate('/login');
-      return;
-    } else {
-      try {
-        // 액세스 토큰을 사용해서 사용자 정보를 가져오는 API 호출
-        const response = await axios.get('https://kapi.kakao.com/v2/user/me', {
-          headers: {
-            Authorization: `Bearer ${access_token}`,
-            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-          },
-        });
-        // API 호출 결과에서 사용자 정보 추출
-        setInfo(response.data);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  };
+  // const getUserInfo = async () => {
+  //   const access_token = sessionStorage.getItem('access_token');
+  //   if (!access_token) {
+  //     //로그인 되어 있지 않다면, 다시 로그인 페이지로
+  //     navigate('/login');
+  //     return;
+  //   } else {
+  //     try {
+  //       // 액세스 토큰을 사용해서 사용자 정보를 가져오는 API 호출
+  //       const response = await axios.get('https://kapi.kakao.com/v2/user/me', {
+  //         headers: {
+  //           Authorization: `Bearer ${access_token}`,
+  //           'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+  //         },
+  //       });
+  //       // API 호출 결과에서 사용자 정보 추출
+  //       setInfo(response.data);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   }
+  // };
 
-  const sendDBKakaoMember = async () => {
-    try {
-      const resCheckId = await axios.post(
-        'http://localhost:4000/register/checkId',
-        {
-          id: info.kakao_account.email,
-        },
-      );
+  // const sendDBKakaoMember = async () => {
+  //   try {
+  //     const resCheckId = await axios.post(
+  //       'http://localhost:4000/register/checkId',
+  //       {
+  //         id: info.kakao_account.email,
+  //       },
+  //     );
 
-      if (resCheckId.status === 200) {
-        const resKakaoRegister = await axios.post(
-          'http://localhost:4000/register',
-          {
-            id: info.kakao_account.email,
-            password: 'none',
-            name: info.properties.nickname,
-            phone: '',
-            addresses: {
-              destination: info.properties.nickname,
-              recipient: info.properties.nickname,
-              address: '',
-              addressDetail: '',
-              zipCode: '',
-              recipientPhone: '',
-              isDefault: true,
-            },
-          },
-        );
-        dispatch(login({ id: info.kakao_account.email }));
-        navigate('/store');
-      }
-    } catch (error) {
-      if (
-        error.response.status === 400 &&
-        error.response.data === '동일한 ID를 가진 회원이 존재합니다.'
-      ) {
-        dispatch(login({ id: info.kakao_account.email }));
-        navigate('/store');
-      }
-    }
-  };
-  useEffect(() => {
-    getUserInfo();
-  }, []);
+  //     if (resCheckId.status === 200) {
+  //       const resKakaoRegister = await axios.post(
+  //         'http://localhost:4000/register',
+  //         {
+  //           id: info.kakao_account.email,
+  //           password: 'none',
+  //           name: info.properties.nickname,
+  //           phone: '',
+  //           addresses: {
+  //             destination: info.properties.nickname,
+  //             recipient: info.properties.nickname,
+  //             address: '',
+  //             addressDetail: '',
+  //             zipCode: '',
+  //             recipientPhone: '',
+  //             isDefault: true,
+  //           },
+  //         },
+  //       );
+  //       dispatch(login({ id: info.kakao_account.email }));
+  //       navigate('/store');
+  //     }
+  //   } catch (error) {
+  //     if (
+  //       error.response.status === 400 &&
+  //       error.response.data === '동일한 ID를 가진 회원이 존재합니다.'
+  //     ) {
+  //       dispatch(login({ id: info.kakao_account.email }));
+  //       navigate('/store');
+  //     }
+  //   }
+  // };
+  // useEffect(() => {
+  //   getUserInfo();
+  // }, []);
 
-  useEffect(() => {
-    if (info.kakao_account) {
-      sendDBKakaoMember();
-    }
-  }, [info]);
+  // useEffect(() => {
+  //   if (info.kakao_account) {
+  //     sendDBKakaoMember();
+  //   }
+  // }, [info]);
 
   return <></>;
 }
