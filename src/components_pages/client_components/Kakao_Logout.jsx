572e6046cd45195c4b8c7d4414270e99ed1e9c60
Drew <drc920630@gmail.com>
2023-05-18 22:16:51 +0900

    로그아웃 기능 완성

--- src/components_pages/client_components/Kakao_Logout.jsx
+++ src/components_pages/client_components/Kakao_Logout.jsx
@@ -1,20 +1,25 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { clickMenu } from '../../store/modules/menuAccount';
 import { logout } from '../../store/modules/user';
 import { reset } from '../../store/modules/cart';
+import { deleteDB } from 'idb';
 
 export default function Kakao_Logout() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  // db닫기 및 삭제
+  const deleteIndex = async () => {
+    await deleteDB('db');
+  };
+
   useEffect(() => {
-    window.sessionStorage.clear(); // 스토리지의 로그인 토큰 삭제
+    deleteIndex();
     dispatch(logout()); // 로그아웃 처리
     dispatch(reset());
     alert('정상적으로 로그아웃 되었습니다!');
-    navigate(`/login`); // 로그인 페이지로 이동
+    navigate(`/store`); // 로그인 페이지로 이동
   }, []);
 
   return <></>;
