80329d83db1540f613d93e66d7d69f51184c9f72
Drew <drc920630@gmail.com>
2023-06-19 21:57:25 +0900

    영상 바꿈

--- src/components_pages/client_components/Intro_movie_clinet.jsx
+++ src/components_pages/client_components/Intro_movie_clinet.jsx
@@ -1,33 +1,40 @@
 /* eslint-disable no-undef */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../../styles/intro_Movie_client.scss';
-import ReactPlayer from 'react-player';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
 
 export default function Intro_movie_client() {
-  const [playING, setPlayING] = useState(false);
+  const videoRef = useRef(null);
 
   useEffect(() => {
-    setPlayING((cur) => true);
+    const player = videojs(videoRef.current, {
+      autoplay: true,
+      muted: true,
+      playsinline: true,
+      preload: 'metadata',
+      loop: true,
+    });
+
+    const handleUser = () => {
+      player.play();
+    };
+
+    document.addEventListener('click', handleUser);
+
+    player.src('/videos/intro.mp4');
 
     return () => {
-      setPlayING((cur) => false);
+      document.removeEventListener('click', handleUser);
+      player.dispose();
     };
   }, []);
 
   return (
     <section className="intro_moive">
       <div className="intro_moive_container">
-        <div className="intro_moive_wrap">
-          <ReactPlayer
-            url="/videos/intro.mp4"
-            width="100%"
-            height="auto"
-            playing={playING}
-            muted={true}
-            loop={playING}
-            volume={0}
-            playsinline={true}
-          />
+        <div data-vjs-player>
+          <video ref={videoRef} className="video-js" width="1682"></video>
         </div>
       </div>
     </section>
