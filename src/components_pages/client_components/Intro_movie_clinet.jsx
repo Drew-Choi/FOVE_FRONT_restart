59971cfc34884ab997c1727bda784306f8f8b04a
Drew <drc920630@gmail.com>
2023-06-20 19:55:13 +0900

    ㅇ

--- src/components_pages/client_components/Intro_movie_clinet.jsx
+++ src/components_pages/client_components/Intro_movie_clinet.jsx
@@ -1,49 +1,14 @@
 /* eslint-disable no-undef */
 import React, { useEffect, useRef, useState } from 'react';
 import '../../styles/intro_Movie_client.scss';
-import videojs from 'video.js';
-import 'video.js/dist/video-js.css';
 
 export default function Intro_movie_client() {
-  // const videoRef = useRef(null);
-
-  // useEffect(() => {
-  //   const player = videojs(videoRef.current, {
-  //     autoplay: true,
-  //     muted: true,
-  //     playsinline: true,
-  //     preload: 'metadata',
-  //     loop: true,
-  //   });
-
-  //   const handleUser = () => {
-  //     player.play();
-  //   };
-
-  //   document.addEventListener('click', handleUser);
-
-  //   player.src('/videos/intro.mp4');
-
-  //   return () => {
-  //     document.removeEventListener('click', handleUser);
-  //     player.dispose();
-  //   };
-  // }, []);
-
   return (
     <section className="intro_moive">
       <div className="intro_moive_container">
-        <video
-          src="/videos/intro.mp4"
-          muted
-          autoPlay
-          loop
-          playsInline
-          className="intro_content"
-        />
-        {/* <div data-vjs-player>
-          <video ref={videoRef} className="video-js" width="1682"></video>
-        </div> */}
+        <video muted playsInline className="intro_content">
+          <source src="/videos/intro.mp4" type="video/mp4" />
+        </video>
       </div>
     </section>
   );
