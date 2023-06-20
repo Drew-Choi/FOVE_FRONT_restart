a9d91d12a80a404e47c9baa0460a5e2ea187261d
Drew <drc920630@gmail.com>
2023-06-20 20:21:35 +0900

    d

--- src/components_pages/client_components/Intro_movie_clinet.jsx
+++ src/components_pages/client_components/Intro_movie_clinet.jsx
@@ -1,14 +1,42 @@
 /* eslint-disable no-undef */
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../../styles/intro_Movie_client.scss';
+import Plyr from 'plyr';
 
 export default function Intro_movie_client() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const player = new Plyr(videoRef.current, {
+      inheritedControls: true,
+      controls: false,
+      autoplay: false,
+      muted: true,
+      volume: 0,
+      preload: 'metadata',
+    });
+    player.source = {
+      type: 'video',
+      sources: [{ src: '/videos/intro.mp4', type: 'video/mp4' }],
+    };
+
+    const handleUser = () => {
+      player.play();
+    };
+
+    document.addEventListener('click', handleUser);
+
+    return () => {
+      document.removeEventListener('click', handleUser);
+      player.destroy();
+    };
+  }, []);
+
   return (
     <section className="intro_moive">
       <div className="intro_moive_container">
-        <video muted playsInline className="intro_content">
-          <source src="/videos/intro.mp4" type="video/mp4" />
-        </video>
+        {' '}
+        <video className="intro_content" ref={videoRef}></video>
       </div>
     </section>
   );
